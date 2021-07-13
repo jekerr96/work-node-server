f984919c6bb2aac9408454234760dab1a952d695
evgenij.astanin <evgenij.astanin@sibirix.ru>
2021-07-13 14:39:42 +0700

    * удаление папок кеша

--- app/clear-cache/index.js
+++ app/clear-cache/index.js
@@ -2,9 +2,7 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function clearCache(query) {
-    console.log(query); //todo remove
-    const {stdout} = await exec('cd ~/../../samba/programmer/projects-svn/' + query.project + '/trunk/www/bitrix/ && ls');
-    console.log(stdout); //todo remove
+    const {stdout} = await exec('cd ~/../../samba/programmer/projects-svn/' + query.project + '/trunk/www/bitrix/ && rm -rf cache managed_cache stack_cache');
 
     return stdout;
 }
