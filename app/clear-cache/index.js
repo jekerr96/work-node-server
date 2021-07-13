dab9db7c40f5ddfeae531e128bbbff88b5ce0dc7
evgenij.astanin <evgenij.astanin@sibirix.ru>
2021-07-13 14:44:05 +0700

    * log

--- app/clear-cache/index.js
+++ app/clear-cache/index.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require('child_process').exec);
 
 async function clearCache(query) {
     const {stdout} = await exec('cd ~/../../samba/programmer/projects-svn/' + query.project + '/trunk/www/bitrix/ && rm -rf cache managed_cache stack_cache');
-
+    console.log(stdout); //todo remove
     return stdout;
 }
 
