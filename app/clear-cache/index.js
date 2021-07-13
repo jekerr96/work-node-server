386da5c18dce36cfb9b8ab16d194ced69bd61192
evgenij.astanin <evgenij.astanin@sibirix.ru>
2021-07-13 14:52:10 +0700

    * try catch

--- app/clear-cache/index.js
+++ app/clear-cache/index.js
@@ -2,9 +2,13 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function clearCache(query) {
-    const {stdout} = await exec('cd ~/../../samba/programmer/projects-svn/' + query.project + '/trunk/www/bitrix/ && rm -rf cache managed_cache stack_cache');
-    console.log(stdout); //todo remove
-    return stdout;
+    try {
+        const {stdout} = await exec('cd ~/../../samba/programmer/projects-svn/' + query.project + '/trunk/www/bitrix/ && rm -rf cache managed_cache stack_cache');
+
+        return stdout;
+    } catch (e) {}
+
+    return "";
 }
 
 module.exports = {
