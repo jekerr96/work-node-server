1ff0699f8d558cd5de60286c0b2db6ae614882a4
evgenij.astanin <evgenij.astanin@sibirix.ru>
2021-07-13 14:35:34 +0700

    * fix path

--- app/clear-cache/index.js
+++ app/clear-cache/index.js
@@ -3,7 +3,7 @@ const exec = util.promisify(require('child_process').exec);
 
 async function clearCache(query) {
     console.log(query); //todo remove
-    const {stdout} = await exec('projects && cd ' + query.project + '/trunk/www/bitrix/ && ls');
+    const {stdout} = await exec('cd ~/../../samba/programmer/projects-svn/' + query.project + '/trunk/www/bitrix/ && ls');
     console.log(stdout); //todo remove
 
     return stdout;
